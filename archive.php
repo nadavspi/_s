5f346ee0acc6e119dbc9f0e0fc8db2f955d32365
Nadav Spiegelman <me@nadav.name>
2014-01-16 18:13:56 -0500

    Remove #primary — just use <main>

--- archive.php
+++ archive.php
@@ -9,8 +9,8 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+
+	<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -98,8 +98,7 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
