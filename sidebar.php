559f65f35c1c096acb8964122bef419716efa84a
Nadav Spiegelman <me@nadav.name>
2014-01-16 15:13:43 -0500

    Add visuallyhidden heading

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,8 @@
  */
 ?>
 	<aside class="sidebar" role="complementary">
+    <h1 class="visuallyhidden">Sidebar</h1>
+    
 		<?php do_action( 'before_sidebar' ); ?>
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 			<?php // default sidebar content can go here ?>
