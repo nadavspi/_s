025626f0518117c36835332dcc5448ac76ff071a
Nadav Spiegelman <me@nadav.name>
2014-01-16 19:07:38 -0500

    Remove #primary — use <main> for content

--- 404.php
+++ 404.php
@@ -7,8 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<main>
 
 			<section class="error-404 not-found">
 				<header class="page__header">
@@ -50,7 +49,6 @@ get_header(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main>
 
 <?php get_footer(); ?>
--- search.php
+++ search.php
@@ -7,8 +7,7 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<main role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -31,8 +30,7 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
